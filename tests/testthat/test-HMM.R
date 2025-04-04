113eeb5610b624e7ec42a76635ee78d60afba4e2
Russell G Almond <russell.g.almond@gmail.com>
2025-04-03 23:12:05 -0400

    Testing Shell for HMM

--- tests/testthat/test-HMM.R
+++ tests/testthat/test-HMM.R
@@ -1,3 +1,46 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("HMM new", {
+
 })
+
+test_that("HMM delT", {
+
+})
+
+test_that("HMM group", {
+
+})
+
+test_that("HMM action", {
+
+})
+
+test_that("HMM task", {
+
+})
+
+test_that("HMM deltaT", {
+
+})
+
+test_that("HMM times", {
+
+})
+
+test_that("HMM dims", {
+
+})
+
+
+test_that("HMM particleFilter", {
+
+})
+
+
+test_that("HMM longResults", {
+
+})
+
+test_that("HMM simulate", {
+
+})
+
