0040eb85a960427ca936e2f4ecb20eb1cecf16e5
Russell G Almond <russell.g.almond@gmail.com>
2025-04-10 18:21:48 -0400

    Adding some plotting functions.

---
+++ R/plots.R
@@ -0,0 +1,45 @@
+bySubjPlot <- function(results,theta0=NULL,
+                       lowcol="gray90",highcol="gray10",
+                       aveshape=4,avecol="red",avesize=2,
+                       trueshape=8,truecol="blue",truesize=2) {
+  summar <- avePart(results)
+  if (!missing(theta0)) summar$theta0 <- theta0
+  plot <-
+    ggplot2::ggplot(results,
+                    ggplot2::aes(x=subj,y=theta,colour=weights,
+                                 frame=occ)) +
+    ggplot2::geom_point() +
+    ggplot2::scale_colour_gradient(low=lowcol,high=highcol) +
+    ggplot2::geom_point(data=summar,
+                        mapping=ggplot2::aes(x=subj,y=theta_bar,frame=occ),
+                        shape=aveshape,col=avecol,size=avesize)
+  if (!missing(theta0))
+    plot <- plot +
+      ggplot2::geom_point(data=summar,
+                          mapping=ggplot2::aes(x=subj,y=theta0,frame=occ),
+                          shape=trueshape,col=truecol,size=truesize)
+  plot
+}
+
+byTimePlot <- function(results,theta0=NULL,
+                       lowcol="gray90",highcol="gray10",
+                       avetype=2,avecol="red",size=1,
+                       truetype=1,truecol="blue") {
+  summar <- avePart(results)
+  if (!missing(theta0)) summar$theta0 <- theta0
+  plot <-
+    ggplot2::ggplot(results,
+                    ggplot2::aes(x=time,y=theta,colour=weights,
+                                 frame=subj)) +
+    ggplot2::geom_point() +
+    ggplot2::scale_colour_gradient(low=lowcol,high=highcol) +
+    ggplot2::geom_line(data=summar,
+                        mapping=ggplot2::aes(x=time,y=theta_bar,frame=subj),
+                        linetype=avetype,col=avecol,size=size)
+  if (!missing(theta0))
+    plot <- plot +
+      ggplot2::geom_line(data=summar,
+                          mapping=ggplot2::aes(x=subj,y=theta0,frame=occ),
+                          linetype=truetype,col=truecol,size=size)
+  plot
+}
